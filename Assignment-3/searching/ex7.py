f75270cc26069cf48f32dd8a679ecce08027f61f
Carla d'Abreu <carlaamvieira@gmail.com>
2021-09-24 23:58:35 +0200

    Update: ex 7

--- Assignment-3/searching/ex7.py
+++ Assignment-3/searching/ex7.py
@@ -1,5 +1,38 @@
 """
-Searching Exercise 7: Search value in mxn Matrix
+Searching Exercise 7: Given an array A[] and a number x, check for pair in A[] with sum as x
 Write a program that, given an array A[] of n numbers and another number x, determines whether or not there exist two
-elements in S whose sum is exactly x.
+elements in A whose sum is exactly x.
 """
+
+# Using set as auxiliary data structure. Time complexity: O(n). Space complexity: O(n).
+# This approach is recommended when is needed to prioritize time complexity other then space complexity
+def is_pair_with_sum1(A, x):
+    # Creates a set to store the complements needed to a number can sum to x.
+    complements = set()
+
+    # for every element in the array, it will check if the element is a complement of a posterior checked number, if
+    # yes, pair founded, if not, its complement wil be added to the set
+    for element in A:
+        if element in complements:
+            return True
+        complements.add(x-element)
+
+    return False
+
+# Using .sort(). Time complexity: O(n*log(n)). Space complexity: O(1).
+# This approach is recommended when is needed to prioritize space complexity other then time complexity
+def is_pair_with_sum2(A, x):
+    # As python uses Tim Sort in .sort(), it will take O(n(log(n)), as n = len(arr)
+    A.sort()
+
+    left, right = 0, len(A)-1
+
+    while left<right:
+        if A[left] + A[right] == x:
+            return True
+        elif A[left] + A[right] < x:
+            left += 1
+        else:
+            right -= 1
+
+    return False
--- Assignment-3/tests/searching/test_ex7.py
+++ Assignment-3/tests/searching/test_ex7.py
@@ -1,9 +1,15 @@
 import unittest
 
+from searching.ex7 import *
 
 class Ex7Test(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_is_pair_with_sum1(self):
+        self.assertTrue(is_pair_with_sum1([0,-1,2,-3,1], -2))
+        self.assertFalse(is_pair_with_sum1([1, -2, 1, 0, 5], 0))
+
+    def test_is_pair_with_sum2(self):
+        self.assertTrue(is_pair_with_sum2([0,-1,2,-3,1], -2))
+        self.assertFalse(is_pair_with_sum2([1, -2, 1, 0, 5], 0))
 
 
 if __name__ == '__main__':
