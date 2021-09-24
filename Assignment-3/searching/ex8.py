1d4dbb04ead3d904364613258b7129f1d9867b8d
Carla d'Abreu <carlaamvieira@gmail.com>
2021-09-25 01:44:28 +0200

    Update: ex 8

--- Assignment-3/searching/ex8.py
+++ Assignment-3/searching/ex8.py
@@ -2,6 +2,30 @@
 Searching Exercise 8: Bonus Question
 Given an array of size n and an integer k, return the count of distinct numbers in all windows of size k.
 
-Input: arr1[] = [11, 1, 13, 21, 3, 7], arr2[] = [11, 3, 7, 1]
-Output: arr2[] is a subset of arr1[]
+
 """
+from collections import defaultdict
+
+# Time complexity: O(n). Space complexity: O(k).
+def count_distinct_elements(arr, k):
+    # the dict current_elements will have the as keys the current elements and as values the current number of
+    # occurrences of that element
+    current_elements = defaultdict(int)
+
+    ans = []
+
+    # it wil pass thought the arr, adding the elements to the dict. if exceeded the window limit, it eliminates the
+    # initial element. for each window, add the amount of unique elements at the moment
+    for index, element in enumerate(arr):
+        if index >= k:
+            discard_element = arr[index-k]
+            current_elements[discard_element] -= 1
+            if current_elements[discard_element] == 0:
+                current_elements.pop(discard_element)
+
+        current_elements[element] += 1
+
+        if index >= k-1:
+            ans.append(len(current_elements))
+
+    return ans
--- Assignment-3/tests/searching/test_ex8.py
+++ Assignment-3/tests/searching/test_ex8.py
@@ -1,9 +1,13 @@
 import unittest
 
+from searching.ex8 import *
+
 
 class Ex8Test(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_count_distinct_elements(self):
+        self.assertEqual(count_distinct_elements([1, 2, 1, 3, 4, 2, 3], 4), [3, 4, 4, 3])
+        self.assertEqual(count_distinct_elements([1, 1, 1, 1, 2, 1, 2, 3], 3), [1, 1, 2, 2, 2, 3])
+        self.assertEqual(count_distinct_elements([1, 2, 4, 4], 2), [2, 2, 1])
 
 
 if __name__ == '__main__':
